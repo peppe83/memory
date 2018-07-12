00078dab0fe10addd4fadfe88408cc39e48d422a
peppe83 <8167474+peppe83@users.noreply.github.com>
2018-07-12 11:19:54 +0200

    4

---
+++ app/src/main/java/com/vimo/memory/activityLogged.java
@@ -0,0 +1,13 @@
+package com.vimo.memory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activityLogged extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged);
+    }
+}
