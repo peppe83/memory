2c8d998ec5c54f0119cfc3cec6b2bb1f162d9b68
peppe83 <8167474+peppe83@users.noreply.github.com>
2018-07-18 12:42:15 +0200

    final version

---
+++ app/src/main/java/com/vimo/memory/data/Constants.java
@@ -0,0 +1,9 @@
+package com.vimo.memory.data;
+
+import android.graphics.Color;
+
+public class Constants {
+
+    public static int COLOR_ICON_GREEN = Color.parseColor("#2cb789");
+    public static int COLOR_ICON_RED = Color.parseColor("#ff0000");
+}
